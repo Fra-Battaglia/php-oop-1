a4a219e03d69261b8962476ea39c71c4901fc613
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-02-04 02:10:45 +0100

    class, construct, movies added

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php 
+	class Movie {
+		public $title;
+		public $cover;
+		public $genre;
+		public $year;
+
+		function __construct($title, $cover, $genre, $year)
+		{
+			$this->title = $title;
+			$this->cover = $cover;
+			$this->genre = $genre;
+			$this->year = $year;
+		}
+
+	}
+	$home_alone = new Movie("Mamma, ho perso l'aereo", "https://m.media-amazon.com/images/I/A1T+lZ6iUZL._SL1500_.jpg", array("comedy", "family"), 1990);
+	$kung_fu_panda = new Movie("Kung Fu Panda", "https://m.media-amazon.com/images/I/517M+F7msHL.jpg", array("cartoon", "comedy", "action"), 2008);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -5,7 +25,8 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>PHP Primo Esercizio OOP</title>
-		<link rel="stylesheet" href="./css/style.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link href="css//style.css" rel="stylesheet" >
 	</head>
 	<body>
 		
